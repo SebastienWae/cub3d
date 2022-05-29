b408c060c3e330e37106d596a99208da17156ea0
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-05-29 19:19:35 +0200

    Add error msg handler

--- src/errors.h
+++ src/errors.h
@@ -1,35 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   errors.c                                           :+:      :+:    :+:   */
+/*   errors.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seb <seb@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/26 22:42:24 by seb               #+#    #+#             */
-/*   Updated: 2022/05/29 12:47:49 by seb              ###   ########.fr       */
+/*   Created: 2022/05/26 22:25:02 by seb               #+#    #+#             */
+/*   Updated: 2022/05/29 09:42:38 by seb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
-#include <stdio.h>
-#include <errno.h>
-#include <errors.h>
-#include <stdlib.h>
+#ifndef ERRORS_H
+# define ERRORS_H
 
-void	error_exit(char *msg)
-{
-	error_msg();
-	if (msg)
-	{
-		ft_putstr_fd(msg, 2);
-		ft_putstr_fd("\n", 2);
-	}
-	exit(EXIT_FAILURE);
-}
+void	error_exit(char *msg);
+void	error_msg(void);
 
-void	error_msg(void)
-{
-	ft_putstr_fd("Error\n", 2);
-	if (errno > 0) // FIXME: false positive?
-		perror(NULL);
-}
+#endif
