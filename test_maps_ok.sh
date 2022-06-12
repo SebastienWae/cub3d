a11e44e75aad41974622f69371dd25fea6338393
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-06-12 10:09:54 +0200

    Add tests runners

---
+++ test_maps_ok.sh
@@ -0,0 +1,7 @@
+./cub3d_debug maps/ok_info_order.cub
+./cub3d_debug maps/ok_minimal.cub
+./cub3d_debug maps/ok_new_lines.cub
+./cub3d_debug maps/ok_offset_wall.cub
+./cub3d_debug maps/ok_offset_wall2.cub
+./cub3d_debug maps/ok_simple.cub
+./cub3d_debug maps/ok_spaces_in_element.cub
